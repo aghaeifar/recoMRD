47a8dc3856db9ca15c6c5ba9a3f97aaf1381dae1
Ali Aghaeifar <ali.aghaeifar@tuebingen.mpg.de>
2022-11-17 13:26:43 +0100

    implementing masking

--- recoMRD_B0.py
+++ recoMRD_B0.py
@@ -17,18 +17,18 @@ class recoMRD_B0(recoMRD):
     def _custom_task(self):
         self.dTE = np.diff(self.xml_hdr.sequenceParameters.TE) * 1e-3
         print(f"Calculating B0 map. dTE = {self.dTE[0]*1e3} ms")
-
         if self.dim_info['rep']['len'] == 1 : #  % regular B0 mapping, b0map = (Eco2 - Eco1)
             self.img_b0 = abs(self.img[:,:,:,:,:,1]) * self.img[:,:,:,:,:,1] * \
                           abs(self.img[:,:,:,:,:,0]) * np.conj(self.img[:,:,:,:,:,0])   
-            self.dim_info['eco']['len'] = 1
-            self.dim_size[self.dim_info['eco']['ind']] = 1
         else: # shims basis-map, b0map = (Eco2Repn - Eco1Repn) - (Eco2Rep1 - Eco1Rep1)
             self.img_b0 = abs(self.img[:,:,:,:,:,1,:]) * self.img[:,:,:,:,:,1,:]          * \
                           abs(self.img[:,:,:,:,:,0,:]) * np.conj(self.img[:,:,:,:,:,0,:]) * \
                           abs(self.img[:,:,:,:,:,1,0]) * np.conj(self.img[:,:,:,:,:,1,0]) * \
                           abs(self.img[:,:,:,:,:,0,0]) * self.img[:,:,:,:,:,0,0]
+            self.img_b0[:,:,:,:,:,0,0] = np.exp( 1j * 1)
 
+        self.dim_info['eco']['len'] = 1
+        self.dim_size[self.dim_info['eco']['ind']] = 1
 
     def get_b0hz(self, scale = 1, offset = 0):
         if self.img_b0_uw.shape != self.img_b0.shape:
@@ -45,14 +45,33 @@ class recoMRD_B0(recoMRD):
 
     def unwrap_b0(self):
         self.img_b0_uw = np.zeros(self.img_b0.shape, dtype=np.float32, order='F')
+        #b0 = np.take(self.img_b0, crep, axis=self.dim_info['rep']['ind'])
+        b0_size = [x for x in self.img_b0.shape if x > 1]
+        if len(b0_size) > 3 :
+            print(f'Only 3D or 4D data is supported for unwrapping. Input shape is {b0_size}')
+            return
+
         dir_path = os.path.dirname(os.path.realpath(__file__))
         handle   = ctypes.CDLL(os.path.join(dir_path, "lib", "libunwrap_b0.so")) 
         handle.unwrap_b0.argtypes = [np.ctypeslib.ndpointer(np.float32, ndim=self.img_b0.ndim, flags='F'),
                                      np.ctypeslib.ndpointer(np.float32, ndim=self.img_b0_uw.ndim, flags='F'),
                                      ctypes.c_int, ctypes.c_int, ctypes.c_int]
-        
-        b0_size = [x for x in self.img_b0.shape if x > 1]
-        handle.unwrap_b0(self.img_b0, self.img_b0_uw, *b0_size)
+        handle.unwrap_b0(self.img_b0, self.img_b0_uw, *b0_size) # 3D and 4D input
+
+    def create_mask(self):
+        self.img_mask = np.zeros(self.img_mag.shape, dtype=np.float32, order='F')
+        mask_size = [x for x in self.img_mag.shape if x > 1]
+        if len(mask_size) > 3 :
+            print(f'Only 3D data is supported for masking. Input shape is {mask_size}')
+            return
+
+        dir_path = os.path.dirname(os.path.realpath(__file__))
+        handle   = ctypes.CDLL(os.path.join(dir_path, "lib", "libbet2.so")) 
+        handle.runBET.argtypes = [np.ctypeslib.ndpointer(np.float32, ndim=self.img_mag.ndim, flags='F'),
+                                  np.ctypeslib.ndpointer(np.float32, ndim=self.img_mask.ndim, flags='F'),
+                                  ctypes.c_int, ctypes.c_int, ctypes.c_int]
+
+        handle.runBET(self.img_mag, self.img_mask, *mask_size) # 3D input
 
     def _coil_combination(self):
         super()._coil_combination()
