6df2825be35291736581e825e189a3084ead9ecc
Ali Aghaeifar <ali.aghaeifar@tuebingen.mpg.de>
2022-11-22 17:13:32 +0100

    update

--- test/test.py
+++ test/test.py
@@ -1,34 +1,20 @@
 # %%
-import recoMRD_B0 
-import importlib
+from recoMRD import recoMRD_B0
+
 import numpy as np 
 import vini
 import os
 
-importlib.reload(recoMRD_B0)
-from recoMRD_B0 import recoMRD_B0
 
-filename = '/DATA/aaghaeifar/rawdata/silent_shimming/data/mrd/meas_MID00575_FID29449_aa_B0Phantom.mrd'
-filename = '/home/ali/Nextcloud/Temp/meas_MID00575_FID29449_aa_B0Phantom.mrd'
+filename = './../example_data/meas_MID00575_FID29449_aa_B0Phantom.mrd'
 mrd = recoMRD_B0(filename)
 
-# %%
 mrd.unwrap_b0()
-vini.show(mrd.img_b0_uw.squeeze())
-
-#%%
-mrd.sqz()
+mrd.create_mask()
+# vini.show(mrd.img_b0_uw.squeeze())
+#vini.show(mrd.img_mask.squeeze())
+#mrd.make_nifti(mrd.img_b0_uw, 'mrd.nii')
 
 
 # %%
-
-import recoMRD_B0 
-import importlib
-import numpy as np 
-import vini
-import os
-
-importlib.reload(recoMRD_B0)
-from recoMRD_B0 import recoMRD_B0
-
 # %%
