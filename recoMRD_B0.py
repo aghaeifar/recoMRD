fc507483c73bbb7236eea67fe59e991d4bf322a6
Ali Aghaeifar <ali.aghaeifar@tuebingen.mpg.de>
2022-11-22 16:28:42 +0100

    eroding mask and change orderings

--- recoMRD_B0.py
+++ recoMRD_B0.py
@@ -1,7 +1,8 @@
-import ctypes
 import os
+import ctypes
 import numpy as np
 from recoMRD import recoMRD
+from scipy import ndimage
 
 class recoMRD_B0(recoMRD):
     dTE         = 0
@@ -44,40 +45,53 @@ class recoMRD_B0(recoMRD):
         self.img_b0_uw  = self.img_b0_uw.squeeze()
 
     def unwrap_b0(self):
-        self.img_b0_uw = np.zeros(self.img_b0.shape, dtype=np.float32, order='F')
-        #b0 = np.take(self.img_b0, crep, axis=self.dim_info['rep']['ind'])
+        print('Unwrapping B0...')
         b0_size = [x for x in self.img_b0.shape if x > 1]
-        if len(b0_size) > 3 :
+        b0_size.append(1)
+        b0_size = b0_size[0:4]
+        if len(b0_size) != 4 :
             print(f'Only 3D or 4D data is supported for unwrapping. Input shape is {b0_size}')
             return
 
         dir_path = os.path.dirname(os.path.realpath(__file__))
         handle   = ctypes.CDLL(os.path.join(dir_path, "lib", "libunwrap_b0.so")) 
         handle.unwrap_b0.argtypes = [np.ctypeslib.ndpointer(np.float32, ndim=self.img_b0.ndim, flags='F'),
-                                     np.ctypeslib.ndpointer(np.float32, ndim=self.img_b0_uw.ndim, flags='F'),
+                                     np.ctypeslib.ndpointer(np.float32, ndim=self.img_b0.ndim, flags='F'),
                                      ctypes.c_int, ctypes.c_int, ctypes.c_int]
-        handle.unwrap_b0(self.img_b0, self.img_b0_uw, *b0_size) # 3D and 4D input
+        b0 = self.img_b0.copy(order='F')        
+        b0_uw = np.zeros(b0.shape, dtype=b0.dtype, order='F')
+        handle.unwrap_b0(b0, b0_uw, *b0_size) # 3D and 4D input  
+        self.img_b0_uw =  b0_uw.copy(order='C') 
+                     
 
-    def create_mask(self):
-        self.img_mask = np.zeros(self.img_mag.shape, dtype=np.float32, order='F')
+    def create_mask(self, erode_size = 3):
+        print('Creating mask...')
         mask_size = [x for x in self.img_mag.shape if x > 1]
-        if len(mask_size) > 3 :
+        if len(mask_size) != 3 :
             print(f'Only 3D data is supported for masking. Input shape is {mask_size}')
             return
 
         dir_path = os.path.dirname(os.path.realpath(__file__))
         handle   = ctypes.CDLL(os.path.join(dir_path, "lib", "libbet2.so")) 
         handle.runBET.argtypes = [np.ctypeslib.ndpointer(np.float32, ndim=self.img_mag.ndim, flags='F'),
-                                  np.ctypeslib.ndpointer(np.float32, ndim=self.img_mask.ndim, flags='F'),
+                                  np.ctypeslib.ndpointer(np.float32, ndim=len(mask_size), flags='F'),
                                   ctypes.c_int, ctypes.c_int, ctypes.c_int]
+        mag = self.img_mag.copy(order='F')
+        mask = np.zeros(mask_size, dtype=mag.dtype, order='F')
+        handle.runBET(mag, mask, *mask_size) # 3D input     
+        if erode_size > 1:            
+            es = erode_size
+            mask = ndimage.binary_erosion(mask, structure=np.ones((es,es,es))).astype(self.img_mask.dtype)
+            mask = np.asfortranarray(mask)  # binary_erosion changes order to C_CONTIGUOUS
 
-        handle.runBET(self.img_mag, self.img_mask, *mask_size) # 3D input
+        self.img_mask =  mask.reshape(self.img_mag.shape).copy(order='C') 
 
     def _coil_combination(self):
         super()._coil_combination()
-        self.img_b0  = np.angle(np.sum(self.img_b0, self.dim_info['cha']['ind'], keepdims=True))
-        self.img_mag = self.img[:,:,:,:,:,0,0,0]
-        self.img = [] # save memory
+        self.img_b0   = np.angle(np.sum(self.img_b0, self.dim_info['cha']['ind'], keepdims=True))
+        self.img_mag  = self.img[:,:,:,:,:,0,0,0].copy()
+        self.img_mask = self.img_mag.copy()
+        # self.img = [] # save memory
                     
 
     
