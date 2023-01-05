4172926696f5887691b40ce63263168926222d11
Ali Aghaeifar <ali.aghaeifar.mri@gmail.com>
2023-01-06 01:50:41 +0330

    adapting to readMRD

--- recoMRD/recoMRD_B0.py
+++ recoMRD/recoMRD_B0.py
@@ -21,31 +21,36 @@ class recoMRD_B0(recoMRD):
         
         self.dTE = np.diff(self.xml_hdr.sequenceParameters.TE) * 1e-3
         print(f"Calculating B0 map. dTE = {self.dTE[0]*1e3} ms")
-        if self.dim_info['rep']['len'] == 1 : #  % regular B0 mapping, b0map = (Eco2 - Eco1)
-            self.img_b0 = abs(self.img[:,:,:,:,:,1]) * self.img[:,:,:,:,:,1] * \
-                          abs(self.img[:,:,:,:,:,0]) * np.conj(self.img[:,:,:,:,:,0])   
+        ind_eco = self.dim_info['eco']['ind']
+        ind_rep = self.dim_info['rep']['ind']
+        if self.dim_info['rep']['len'] == 1 : #  % regular B0 mapping, b0map = (Eco2 - Eco1)  
+            # using .take, we used [1] rather than 1 to specify slice index. this let to keep that singleton dimension. Otherwise we should expand dims to keep consistency.          
+            self.img_b0 = abs(self.img.take([1],axis=ind_eco)) * self.img.take([1],axis=ind_eco) * \
+                          abs(self.img.take([0],axis=ind_eco)) * np.conj(self.img.take([0],axis=ind_eco))   
         else: # shims basis-map, b0map = (Eco2Repn - Eco1Repn) - (Eco2Rep1 - Eco1Rep1)
-            self.img_b0 = abs(self.img[:,:,:,:,:,1,:]) * self.img[:,:,:,:,:,1,:]          * \
-                          abs(self.img[:,:,:,:,:,0,:]) * np.conj(self.img[:,:,:,:,:,0,:]) * \
-                          abs(self.img[:,:,:,:,:,1,0]) * np.conj(self.img[:,:,:,:,:,1,0]) * \
-                          abs(self.img[:,:,:,:,:,0,0]) * self.img[:,:,:,:,:,0,0]
+            self.img_b0 = abs(self.img.take([1],axis=ind_eco)) * self.img.take([1],axis=ind_eco)          * \
+                          abs(self.img.take([0],axis=ind_eco)) * np.conj(self.img.take([0],axis=ind_eco)) * \
+                          abs(self.img.take([1],axis=ind_eco).take([0],axis=ind_rep)) * np.conj(self.img.take([1],axis=ind_eco).take([0],axis=ind_rep)) * \
+                          abs(self.img.take([0],axis=ind_eco).take([0],axis=ind_rep)) * self.img.take([0],axis=ind_eco).take([0],axis=ind_rep)
             self.img_b0[:,:,:,:,:,0,0] = np.exp( 1j * 1)
 
-        self.dim_info['eco']['len'] = 1
-        self.dim_size[self.dim_info['eco']['ind']] = 1
+        self.dim_info['eco']['len'] = self.dim_info['eco']['len'] - 1
+        self.dim_size[self.dim_info['eco']['ind']] = self.dim_info['eco']['len'] - 1
+
+        self.coil_combination(self.img, method='sos', coil_sens=None, update_diminfo=True)
 
     ##########################################################
-    def get_b0hz(self, scale = 1, offset = 0):
+    def get_b0hz(self, offset = 0):
         if self.img_b0_uw.shape != self.img_b0.shape:
             print(f"\033[93mUnwrapped image is not yet calculated. \033[0m")
             return None
-        return (scale*self.img_b0_uw + offset) / self.dTE[0] / (2*np.pi)
+        return (self.img_b0_uw + offset) / self.dTE[0] / (2*np.pi)
 
     ##########################################################
     def unwrap_b0(self):
         print('Unwrapping B0...')
         b0_size = [x for x in self.img_b0.shape if x > 1]
-        b0_size.append(1)
+        b0_size.append(1) # unwrapper assumes 4D inputs and loops over 4th dimensions. We set the 4th dim to 1, in case of 3D data.
         b0_size = b0_size[0:4]
         if len(b0_size) != 4 :
             print(f'Only 3D or 4D data is supported for unwrapping. Input shape is {b0_size}')
@@ -85,11 +90,10 @@ class recoMRD_B0(recoMRD):
 
         self.img_mask =  mask.reshape(self.img_mag.shape).copy(order='C') 
 
-    def _coil_combination(self, volume, method='sos', coil_sens=None):
-        self.img = super()._coil_combination(self.img, method='sos')
-        self.img_b0   = np.angle(np.sum(self.img_b0, self.dim_info['cha']['ind'], keepdims=True))
-        self.img_mag  = self.img[:,:,:,:,:,0,0,0].copy()
+    def coil_combination(self, volume, method='sos', coil_sens=None, update_diminfo=False):
+        self.img_mag  = super().coil_combination(volume, method='sos')[:,:,:,:,:,0:1,0:1,0:1,0:1,0:1,0:1].copy() # I used 0:1 rather than  0 fro indexing to keep singleton dimensions. See https://stackoverflow.com/questions/3551242
         self.img_mask = self.img_mag.copy()
+        self.img_b0   = np.angle(np.sum(self.img_b0, self.dim_info['cha']['ind'], keepdims=True))
         # self.img = [] # save memory
                     
 
